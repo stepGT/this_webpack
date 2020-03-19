16ea70ceb0cb2487b922dc47a98ba821629a3039
Andrey <stepgt@mail.ru>
2020-03-19 11:55:55 +1000

    Init project

---
+++ src/analytics.js
@@ -0,0 +1,25 @@
+/**
+ *
+ * @returns {{destroy: (function())}}
+ */
+function createAnalytics() {
+  let counter = 0;
+  let isDestroyed = false;
+  const listener = () => counter++;
+
+  document.addEventListener('click', listener);
+  return {
+    destroy() {
+      document.removeEventListener('click', listener);
+      isDestroyed = true;
+    },
+    getClicks() {
+      if (isDestroyed) {
+        return 'Analytics is destroyed';
+      }
+      return counter;
+    }
+  }
+}
+
+window.analytics = createAnalytics();
